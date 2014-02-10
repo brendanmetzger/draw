f4b8cf5f590b7630f31cc47b3bac365a8a777611
Brendan <bmetzger@wbez.org>
2014-02-10 11:55:13 -0600

    Making progress svg donut

---
+++ progress/README.md
@@ -0,0 +1,4 @@
+# progress
+
+## Donut
+A pretty standard thing we see - I've made a simple class to draw one, and in it's most pure state, it provides you `update` method that takes a ratio (float) from 0 to 1, indicative  completeness.
---
+++ progress/donut.js
@@ -0,0 +1,56 @@
+var ProgressDonut = function(embed_container, size, css) {
+  var svg, mask, mask_elem, complete, path;
+  
+  // these methods are exposed to the user,
+  this.createElement = function(name, opt, parent) {
+    var node = document.createElementNS('http://www.w3.org/2000/svg', name);
+    for (var key in opt) {
+      node.setAttribute(key, opt[key] instanceof SVGMaskElement ? 'url(#'+opt[key].id+')' : opt[key]);
+    }
+    return parent ? parent.appendChild(node) : svg.appendChild(node);
+  };
+  
+  this.update = function(percentage) {
+    var radian = (2 * Math.PI) * percentage;
+    var x = (Math.cos(radian) * 50) + 50;
+    var y = ((Math.sin(radian) - 1e-5) * 50) + 50;
+    return path.setAttribute('d', "M50,50 l50,0 A50,50 0 " + (y < 50 ? 1 : 0) + "1 " + x + "," + y + "z");
+  };
+
+  svg = this.createElement('svg', {
+    'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+    version: 1.1,
+    height: size,
+    width: size,
+    style: css,
+    viewBox: '0 0 100 100'
+  }, embed_container);
+
+  // This is the mask for the donut shape - I've left the code terse, so please consider removing or making it your own.
+  mask = this.createElement('mask', {
+    'id': 'cutout'
+  }, this.createElement('defs'));
+  
+  mask_elem = this.createElement('circle', {
+    'cx': 50,
+    'cy': 50,
+    'r': 50,
+    'style': 'fill:#000;stroke:#fff;stroke-width:50;'
+  }, mask);
+
+  complete = this.createElement('circle', {
+    'cx': 50,
+    'cy': 50,
+    'r': 50,
+    'style': 'fill:#f8f8f8',
+    'mask': mask
+  });
+
+  path = this.createElement('path', {
+    'd': 'M50,50',
+    'transform': 'rotate(-90 50 50)',
+    'mask': mask
+  });
+
+  return this;
+};
