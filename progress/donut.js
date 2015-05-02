b69bde167f93adcb91f3f0e6fef04b283c21b2c5
Brendan <brendan.metzger@gmail.com>
2015-05-02 08:27:12 -0500

    updates

--- progress/donut.js
+++ progress/donut.js
@@ -1,5 +1,5 @@
 var ProgressDonut = function(embed_container, size, css) {
-  var svg, mask, mask_elem, complete, path;
+  var svg, mask, mask_elem, complete, path, animatePath;
   
   // these methods are exposed to the user,
   this.createElement = function(name, opt, parent) {
@@ -9,12 +9,18 @@ var ProgressDonut = function(embed_container, size, css) {
     }
     return parent ? parent.appendChild(node) : svg.appendChild(node);
   };
+
   
   this.update = function(percentage) {
     var radian = (2 * Math.PI) * percentage;
     var x = (Math.cos(radian) * 50) + 55;
-    var y = ((Math.sin(radian) - 1e-5) * 50) + 55;
-    return path.setAttribute('d', "M55,55 l50,0 A50,50 0 " + (y < 50 ? 1 : 0) + "1 " + x + "," + y + "z");
+    var y = (Math.sin(radian) * 50) + 55;
+    var from = animatePath.getAttribute('to');
+    var to = "M55,55 l50,0 A50,50 0 " + (y < 55 ? 1 : 0) + "1 " + x + "," + y;
+    animatePath.setAttribute('from', from);
+    animatePath.setAttribute('to', to);
+    animatePath.beginElement();
+    return path.setAttribute('d', to);
   };
 
   svg = this.createElement('svg', {
@@ -47,10 +53,19 @@ var ProgressDonut = function(embed_container, size, css) {
   });
 
   path = this.createElement('path', {
-    'd': 'M55,55',
+    'd': 'M55,55 l50,0 A50,50 0 01 105,55',
     'transform': 'rotate(-90 55 55)',
     'mask': mask
   });
+
+  animatePath = this.createElement('animate', {
+    'attributeName': 'd',
+    'attributeType': 'XML',
+    'from': 'M55,55 l50,0 A50,50 0 01 105,55',
+    'to': 'M55,55 l50,0 A50,50 0 01 105,55',
+    'dur': '5s',
+    'fill': 'freeze'
+  }, path);
 
   return this;
 };
